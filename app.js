4a97264ef975cde93204bd4dee31cebe992fb42b
attheodo <at@atworks.gr>
2015-04-22 11:51:45 +0300

    Better error handling of the promise chain

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ var express = require('./config/express.js');
 
 var app = express.app;
 
-var log = app.get('winston-logger');
+var l = app.get('winston-logger');
 var config = app.get('nconf');
 
 var bootstrap = require('./config/bootstrap.js')(app);
 
 express
-.setup()
-.then(bootstrap)
-.then(function() {
-    var server = app.listen(config.get('server:port'), function () {
-      console.log('\ninfo:'.green +' [Orient] Listening at http://%s:%s', server.address().address, server.address().port);
-    });
-});
+  .setup()
+  .then(bootstrap)
+  .then(function() {
+      var server = app.listen(config.get('server:port'), function () {
+        console.log('\ninfo:'.green +' [Orient] Listening at http://%s:%s', server.address().address, server.address().port);
+      });
+  })
+  .catch(function(err){
+    l.error(err.message);
+    process.exit(-1);
+  })
