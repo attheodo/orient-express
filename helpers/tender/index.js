3f0b9c2dc967c17b1fc5af6f91e0a6c93b30d6a5
attheodo <at@atworks.gr>
2015-04-22 12:36:27 +0300

    minor refactoring and improvmenets

--- helpers/tender/index.js
+++ helpers/tender/index.js
@@ -1,9 +1,11 @@
-/*
+ /*
 *   Tender attaches models to Orient-Express using
 *   the Waterline ORM
 *
 *   https://github.com/balderdashy/waterline
 */
+
+/* jshint latedef: false */
 
 var path  = require('path');
 var fs    = require('fs');
@@ -26,7 +28,9 @@ module.exports = function(app) {
     var adapters = {};
 
     function init() {
+
         return new BPromise(function(resolve, reject) {
+
             var modelsPath = path.resolve(path.dirname(require.main.filename) + config.modelsPath);
 
             var waterline = new Waterline();
@@ -38,7 +42,6 @@ module.exports = function(app) {
 
             fs.readdirAsync(modelsPath).map(function(filename) {
 
-                var adapterModule = null;
                 var filePath = modelsPath + '/' + filename;
 
                 if (path.extname(filename) !== '.js') {
@@ -46,69 +49,28 @@ module.exports = function(app) {
                 }
 
                 var model = require(filePath);
+                sanitizeModelConfiguration(config, adapters, filename, model);
 
-                if(!_.isString(model.identity)) {
-                    model.identity = filename.split('.')[0];
-                }
-
-                if(!_.isString(model.connection)) {
-                    model.connection = 'defaultDatabase';
-                }
-
-                // don't touch models if we're on production
-                if(!_.isUndefined(model.migrate) && process.env.NODE_ENV === 'production') {
-                    model.migrate = 'safe';
-                }
-
-                if(_.isUndefined(model.adapter) && _.isUndefined(config.waterline.connections[model.connection].adapter)) {
-                    throw new Error('[Tender] There\'s no default adapter for connection "' +
-                    model.connection+'", nor an explicitly configured adapter in "' +
-                    model.identity+'.js"');
-                }
-                // give priority to the explicitly set adapter in model configuration file
-                else if (!_.isUndefined(model.adapter)) {
-
-                    try {
-
-                        adapterModule = require(model.adapter);
-                        adapters[model.adapter] = adapterModule;
-
-                    } catch(e) {
-                        throw new Error('[Tender] Connection adapter module "'+model.adapter+
-                          '" defined in "'+model.identity+'.js'+'" not found. Try \'npm install ' +
-                          model.adapter+' --save\'');
-                    }
-
-                }
-                // fallback to default adapter for connection
-                else {
-                    try {
-
-                        var adapter = config.waterline.connections[model.connection].adapter;
-                        adapterModule = require(adapter);
-                        adapters[model.adapter] = adapterModule;
-
-                    } catch(e) {
-                        throw new Error('[Tender] Default Connection adapter module "'
-                        +adapter+'" for connection "'+model.connection+
-                        '" not found. Try \'npm install '+adapter+' --save\'');
-                    }
-                }
 
                 var collection = Waterline.Collection.extend(model);
                 waterline.loadCollection(collection);
 
                 if (v) {
-                    l.info('[Tender]'+' ✓'.green +'  Loaded model "'+model.identity.underline+'" with connection "'+model.adapter+':'+model.connection+'"');
+                    l.info('[Tender]'+' ✓'.green +'  Loaded model "'+model.identity.underline +
+                    '" with connection "'+model.adapter+':'+model.connection+'"');
                 }
 
-            }, {concurrency: 5}).then(function() {
+            }, {concurrency: 5})
+
+            .then(function() {
 
                 config.waterline.adapters = adapters;
 
                 if(_.isUndefined(config.migrate)) {
+
                     l.warn('[Tender] It appears you\'ve not set a model migration setting in your global configuration file. Assuming "migrate": "safe".');
                     config.migrate = 'safe';
+
                 } else {
 
                     // don't let waterline mess with the models in any way if we're on production!
@@ -122,15 +84,26 @@ module.exports = function(app) {
                 }
 
                 return waterline.initializeAsync(config.waterline);
-            }).then(function(data) {
+
+          })
+          .then(function(data) {
+
                 resolve({models: data.collections, connections: data.connections});
-            }).catch(Error, function(err) {
-                if (err.code === 'ENOENT') {
-                    reject('[Tender] Error reading model definition files: '+ err);
-                } else {
-                    reject(err);
-                }
-            });
+
+          })
+          .catch(Error, function(err) {
+
+              if (err.code === 'ENOENT') {
+
+                  reject('[Tender] Error reading model definition files: '+ err);
+
+              } else {
+
+                  reject(err);
+              }
+
+          });
+
         });
     }
 
@@ -138,3 +111,57 @@ module.exports = function(app) {
         init: init
     };
 };
+
+function sanitizeModelConfiguration(config, adapters, filename, model) {
+
+  var adapterModule = null;
+
+  if(!_.isString(model.identity)) {
+      model.identity = filename.split('.')[0];
+  }
+
+  if(!_.isString(model.connection)) {
+      model.connection = 'defaultDatabase';
+  }
+
+  // don't touch models if we're on production
+  if(!_.isUndefined(model.migrate) && process.env.NODE_ENV === 'production') {
+      model.migrate = 'safe';
+  }
+
+  if(_.isUndefined(model.adapter) && _.isUndefined(config.waterline.connections[model.connection].adapter)) {
+      throw new Error('[Tender] There\'s no default adapter for connection "' +
+      model.connection+'", nor an explicitly configured adapter in "' +
+      model.identity+'.js"');
+  }
+  // give priority to the explicitly set adapter in model configuration file
+  else if (!_.isUndefined(model.adapter)) {
+
+      try {
+
+          adapterModule = require(model.adapter);
+          adapters[model.adapter] = adapterModule;
+
+      } catch(e) {
+          throw new Error('[Tender] Connection adapter module "'+model.adapter+
+            '" defined in "'+model.identity+'.js'+'" not found. Try \'npm install ' +
+            model.adapter+' --save\'');
+      }
+
+  }
+  // fallback to default adapter for connection
+  else {
+      try {
+
+          var adapter = config.waterline.connections[model.connection].adapter;
+          adapterModule = require(adapter);
+          adapters[model.adapter] = adapterModule;
+
+      } catch(e) {
+          throw new Error('[Tender] Default Connection adapter module "' +
+          adapter+'" for connection "'+model.connection+
+          '" not found. Try \'npm install '+adapter+' --save\'');
+      }
+  }
+
+}
